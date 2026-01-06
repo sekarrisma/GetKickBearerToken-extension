ae41c9e915f9bf5e9f3062c3797d1c9d9ee0b37e
titoinemb <mb_titoine@proton.me>
2026-01-06 15:08:43 -0500

    build: remove build dir

--- build/content.js
+++
@@ -1,1 +0,0 @@
-"use strict";let cookie=document.cookie.split("; ").find(e=>e.startsWith("session_token="));if(cookie){let e=decodeURIComponent(cookie.split("=")[1]);chrome.runtime.sendMessage({session_token:e})}
